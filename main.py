9e532763f17825350cc1eded6d58a156f88b3e3d
MauroAntonioBogado1990 <mauroantoniobogado@gmail.com>
2022-10-04 11:41:08 -0300

    Add to class an dfunc show

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
+from faulthandler import cancel_dump_traceback_later
+from unicodedata import name
 from fastapi import FastAPI
-
+from pydantic import BaseModel#creamos la base de model 
+from typing import Optional #importamos para generar el valor de id
 #creamos la aplicacion para comenzar
 
 app = FastAPI()
 
 #publicaciones donde se va a guardar los elementos
-posts = []
+elemnts = []
+
+#creamos la clase
+class Element(BaseModel):
+    id : Optional[str]
+    name : str
+    cant : int
+    color : str
 
 #creacion de la ruta de indices
 @app.get('/')
@@ -15,13 +25,14 @@ def read_root():
 #creamos la ruta de las publicaciones
 @app.get('/posts')
 def show_posts():
-    return posts
+    return elemnts
 
 
 #creamos ruta de la creacion de publicaciones
 @app.post('/posts')
-def create_posts(post):
-    print(posts)
+def create_posts(post: Element):
+    elemnts.append(post.dict())#agregamos el elemento al arreglo
+    print(post.dict())#crea la manera de que se vea como diccionario de clave-valor
     return "recieved"
 
 
