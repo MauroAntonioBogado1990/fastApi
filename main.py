5deda367f6c9a523bb23c3bd4b3bb3e3029acc45
MauroAntonioBogado1990 <mauroantoniobogado@gmail.com>
2022-10-07 13:34:17 -0300

    add to function post, post/id

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from unicodedata import name
 from fastapi import FastAPI
 from pydantic import BaseModel#creamos la base de model 
 from typing import Optional #importamos para generar el valor de id
+from uuid import uuid4 as uuid#importamos para tener el id unico
 #creamos la aplicacion para comenzar
 
 app = FastAPI()
@@ -31,8 +32,20 @@ def show_posts():
 #creamos ruta de la creacion de publicaciones
 @app.post('/posts')
 def create_posts(post: Element):
+    post.id = str(uuid())#creamos el id unico
     elemnts.append(post.dict())#agregamos el elemento al arreglo
     print(post.dict())#crea la manera de que se vea como diccionario de clave-valor
-    return "recieved"
+    return elemnts[-1]
 
+#creamos get de retorno de id
+@app.get('/posts/{post_id}')
+def get_post_id(post_id :str):
+    for e in elemnts:
+        if e["id"] == post_id:
+            return post_id
+    return ('not found')
+     
+
+
+    
 
